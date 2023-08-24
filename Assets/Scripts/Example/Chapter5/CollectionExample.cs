03fdc23d2522fb5dc94a76a46a5536aafa2eb8a8
ErrorMonkey <puts.hello.world.return0@gmail.com>
2023-08-24 13:06:08 +0900

    유니티 개발 연습

---
+++ Assets/Scripts/Example/Chapter5/CollectionExample.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CollectionExample : MonoBehaviour
+{
+	// * 배열의 비효율성
+	// - 크기를 변경할 수 없다.
+
+	int[] numbers = new int[5];
+
+    private void Start()
+    {
+        numbers[0] = 1;
+        numbers = new int[6];
+    }
+}
+
